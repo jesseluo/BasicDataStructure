5c6d879ac7192e00a98ee6f357fc00e434b7b17f
Jesse Luo <radishchrist@gmail.com>
2015-07-26 22:38:39 +0800

    Initial Commit

---
+++ BasicDataStructure/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BasicDataStructure
+//
+//  Created by Jesse Luo on 15/7/26.
+//  Copyright © 2015年 Jesse Luo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
